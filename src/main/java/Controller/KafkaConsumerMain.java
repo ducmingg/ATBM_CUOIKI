85de50836a03ce01b8957bbbbffc2dfdd15a76b3
duckies <voducminh39@gmail.com>
2024-12-15 21:05:27 +0700

    fix conflig

--- src/main/java/Controller/KafkaConsumerMain.java
+++ src/main/java/Controller/KafkaConsumerMain.java
@@ -15,9 +15,9 @@ public class KafkaConsumerMain {
 
     public static void main(String[] args) {
         // Cấu hình Kafka Consumer
-        String bootstrapServers = "192.168.1.4:9094"; // Thay đổi với địa chỉ Kafka Broker của bạn
-        String groupId = "test-group";  // Thay đổi với Group ID của bạn
-        String topic = "db.webbds.orderitem_logs"; // Thay đổi với tên topic bạn muốn tiêu thụ
+        String bootstrapServers = "192.168.1.4:9094";
+        String groupId = "test-group";
+        String topic = "db.webbds.orderitem_logs";
 
         // Cấu hình các tham số cho Consumer
         Properties properties = new Properties();
@@ -25,17 +25,15 @@ public class KafkaConsumerMain {
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest"); // Chỉ định vị trí bắt đầu đọc nếu không có offset
+        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
-        // Tạo Consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 
-        // Đăng ký với topic
         consumer.subscribe(List.of(topic));
         EmailService emailService = new EmailService();
         try {
             while (true) {
-                var records = consumer.poll(java.time.Duration.ofMillis(100)); // Chờ 100ms để lấy tin nhắn
+                var records = consumer.poll(java.time.Duration.ofMillis(100));
                 records.forEach(record -> {
                     JSONObject valueJson = new JSONObject(record.value());
                     JSONObject payload = valueJson.optJSONObject("payload");
@@ -57,7 +55,7 @@ public class KafkaConsumerMain {
                 });
             }
         } finally {
-            consumer.close(); // Đảm bảo đóng consumer khi kết thúc
+            consumer.close();
         }
     }
 }
